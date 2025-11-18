d266387ea60d962b6a9f468ccedc3bfbc935b4d4
ibraahim <ibraakadarba.12@gmail.com>
2025-11-18 08:25:13 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_chroma import Chroma
+from langchain_community.vectorstores import Chroma as CommunityChroma  # only if needed
+
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+st.title("RAG Application using Gemini Pro")
+
+loader = PyPDFLoader("Dastuurka_ku_meelgaarka_SOM_03092012-1_2.pdf")
+data = loader.load()
+
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000)
+docs = text_splitter.split_documents(data)
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vectorstore = Chroma.from_documents(documents=docs, embedding=embeddings)
+
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 10})
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0,max_tokens=None,timeout=None)
+
+
+query = st.chat_input("Ask me anything: ") 
+prompt = query
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+) 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+if query:
+    question_answer_chain = create_stuff_documents_chain(llm, prompt)
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+    response = rag_chain.invoke({"input": query})
+    print(response["answer"])
+
+    st.write(response["answer"])
